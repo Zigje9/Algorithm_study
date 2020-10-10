c07c5b3f0455cd20b5c7a916493e94596b44be4e
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-10-10 19:06:35 +0900

    N&M 6

---
+++ python/15655.py
@@ -0,0 +1,30 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+num_line = sys.stdin.readline()
+num_arr = sorted(list(map(int, num_line.split())))
+visit = []
+for i in range(N):
+    visit.append(0)
+
+
+def dfs(count, answer):
+    if count == 0:
+        print(' '.join(answer))
+        return
+    for idx in range(N):
+        if visit[idx] == 0:
+            if len(answer) == 0:
+                visit[idx] = 1
+                answer.append(str(num_arr[idx]))
+                dfs(count-1, answer)
+                answer.pop()
+                visit[idx] = 0
+            else:
+                if int(answer[-1]) < num_arr[idx]:
+                    visit[idx] = 1
+                    answer.append(str(num_arr[idx]))
+                    dfs(count - 1, answer)
+                    answer.pop()
+                    visit[idx] = 0
+
+dfs(M, [])

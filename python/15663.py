e69cb6a325102981f872e15c3911ff49bcc5c32d
pjkwprn@gmail.com <pjkwprn@gmail.com>
2020-10-19 16:05:26 +0900

    N&M 9

---
+++ python/15663.py
@@ -0,0 +1,25 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+
+num_list = sorted(list(map(int, sys.stdin.readline().split())))
+
+visit = [0 for i in range(N)]
+
+
+def bfs(start, answer):
+    if start == M:
+        print(' '.join(map(str, answer)))
+        return
+    check = -1
+    for idx in range(N):
+        if visit[idx] == 0:
+            if check != num_list[idx]:
+                answer.append(num_list[idx])
+                check = num_list[idx]
+                visit[idx] = 1
+                bfs(start+1, answer)
+                answer.pop(-1)
+                visit[idx] = 0
+
+
+bfs(0, [])

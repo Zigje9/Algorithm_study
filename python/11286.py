f99bf44a47d27580e49848b8fbc34209c1b83940
jekoo <pjkwprn@gmail.com>
2021-03-24 15:25:45 +0900

    절댓값 힙

---
+++ python/11286.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+
+heap = []
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(num), num))

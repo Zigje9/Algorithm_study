d7122f77540b4a552c3f1d1365d704ad6f729185
jekoo <pjkwprn@gmail.com>
2021-03-30 12:48:04 +0900

    최소, 최대2

---
+++ python/20053.py
@@ -0,0 +1,14 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+
+def solution():
+    N = int(sys.stdin.readline())
+    numlist = list(map(int, sys.stdin.readline().split()))
+    print(min(numlist), max(numlist))
+
+
+while T > 0:
+    solution()
+    T -= 1

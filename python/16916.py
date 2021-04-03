58ff8f80feab202b4272db8b8b3a7e2ccac0968a
jekoo <pjkwprn@gmail.com>
2021-04-04 00:03:42 +0900

    부분 문자열

---
+++ python/16916.py
@@ -0,0 +1,52 @@
+import sys
+
+S = sys.stdin.readline().rstrip()
+P = sys.stdin.readline().rstrip()
+
+N = len(P)
+
+lps_array = [0]*(N)
+
+answer = []
+
+
+def lps(P, N):
+    j = 0
+    i = 1
+    while i < N:
+        if P[i] == P[j]:
+            j += 1
+            lps_array[i] = j
+            i += 1
+        else:
+            if j == 0:
+                lps_array[i] = 0
+                i += 1
+            else:
+                j = lps_array[j-1]
+
+
+def KMP(S, P, lps_array):
+    j = 0
+    i = 0
+    while i < len(S):
+        if S[i] == P[j]:
+            i += 1
+            j += 1
+        else:
+            if j == 0:
+                i += 1
+            else:
+                j = lps_array[j-1]
+
+        if j == len(P):
+            answer.append(i-len(P))
+            return
+
+
+lps(P, N)
+KMP(S, P, lps_array)
+if len(answer) == 0:
+    print(0)
+else:
+    print(1)

c2e1e35ad2e3222074067ab46c63071d88a1aa8e
jekoo <pjkwprn@gmail.com>
2021-08-29 15:31:23 +0900

    휴게소 세우기

---
+++ python/1477.py
@@ -0,0 +1,29 @@
+import sys
+
+N, M, L = map(int, sys.stdin.readline().split())
+
+rest = list(map(int, sys.stdin.readline().split()))
+
+rest.append(0)
+rest.append(L-1)
+rest.sort()
+
+left = 0
+right = L-1
+while left <= right:
+    mid = (left + right) // 2
+    count = 0
+    for i in range(N+1):
+        if (rest[i+1]-rest[i]) > mid:
+            count += (rest[i+1]-rest[i]-1) // mid
+    if count > M:
+        left = mid + 1
+    else:
+        answer = mid
+        right = mid - 1
+
+
+print(answer)
+
+
+
